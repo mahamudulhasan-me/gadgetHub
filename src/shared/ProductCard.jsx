d2fe935a2ee19cc787c402218053b48bf16c143a
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-08-02 17:45:18 +0600

    add dyanmic rating

--- src/shared/ProductCard.jsx
+++ src/shared/ProductCard.jsx
@@ -1,14 +1,40 @@
+import { Rating, ThinStar } from "@smastrom/react-rating";
+
 const ProductCard = ({ product }) => {
-  const { id, title, price, description, category, thumbnail } = product;
+  const {
+    short_features,
+    title,
+    price,
+    rating,
+    total_rating,
+    description,
+    category,
+    thumbnail,
+  } = product;
+  const myStyles = {
+    itemShapes: ThinStar,
+    activeFillColor: "#003D2A",
+    inactiveFillColor: "#BCEDC5",
+  };
   return (
-    <div className="flex flex-col justify-between items-center">
+    <div className="flex flex-col justify-between">
       <figure className="w-full bg-[#F5F6F6] justify-center items-center">
         <img src={thumbnail} alt="" />
       </figure>
       <div>
-        <h1>
-          <span>{title}</span> {price}
+        <h1 className="flex font-semibold justify-between gap-1">
+          <span>{title}</span> <span>${price}</span>
         </h1>
+        <p className="text-sm text-gray-600">{short_features}</p>
+        <p className="flex">
+          <Rating
+            style={{ maxWidth: 100 }}
+            value={rating}
+            readOnly
+            itemStyles={myStyles}
+          />
+          <span>({total_rating})</span>
+        </p>
       </div>
     </div>
   );
